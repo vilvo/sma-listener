9cf8c0cd3c3831929b9aabb3b24c487248846cd4
Ville Ilvonen <vilvo@iki.fi>
2024-02-05 19:24:28 +0200

    pcap: capture SHM2 udp multicast from the main IF
    Signed-off-by: Ville Ilvonen <vilvo@iki.fi>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 
 fn main() {
-    for device in pcap::Device::list().expect("device lookup failed")
-    {   
-        println!("Found device {:?}", device);
+
+    let mut capture = pcap::Capture::from_device("wlp1s0f0")
+        .unwrap()
+        .immediate_mode(true)
+        .open().unwrap();
+
+    let filter_str: &str = "udp dst port 9522";
+    capture.filter(filter_str, true).unwrap();
+
+    while let Ok(packet) = capture.next_packet() {
+        println!("{:?}", packet);
     }
 }
