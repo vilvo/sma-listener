86a709920b9262d6aab22f75707ae866e45f342e
Ville Ilvonen <vilvo@iki.fi>
2024-02-05 20:09:55 +0200

    test: filter "SMA" in data
    Signed-off-by: Ville Ilvonen <vilvo@iki.fi>

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,16 @@ fn main() {
     let filter_str: &str = "udp dst port 9522";
     capture.filter(filter_str, true).unwrap();
 
+    let mut savefile = capture.savefile("test.pcap").unwrap();
+
     while let Ok(packet) = capture.next_packet() {
-        println!("{:?}", packet);
+        if packet.data[42] == b'S' &&
+            packet.data[43] == b'M' &&
+            packet.data[44] == b'A'
+        {
+            println!("{}", "SMA");
+            savefile.write(&packet);
+            break;
+        }
     }
 }
